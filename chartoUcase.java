20a04236a60bb784c6bd81c58bd826f6eb16b475
RehanAli2006 <rehanali195925@gmail.com>
2025-08-06 22:40:49 +0530

    char  to uppercase
    char to uppercase

---
+++ chartoUcase.java
@@ -0,0 +1,23 @@
+public class chartoUcase {
+    public static String characterintouppercase(String str){
+        StringBuilder sb=new StringBuilder("");
+        char ch=Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+       for(int i=1;i<str.length();i++ ){
+        if(str.charAt(i)==' ' && i<str.length()-1){
+            sb.append(str.charAt(i));
+            i++;//hiu
+            sb.append(Character.toUpperCase(str.charAt(i)));
+        }else{
+            sb.append(str.charAt(i));
+        }
+       }
+            return sb.toString();
+        
+    }//hi
+//hlo
+    public static void main(String args[]){
+        String str="hi, i am rehan ali";
+        System.out.println(characterintouppercase(str));
+    }
+}
